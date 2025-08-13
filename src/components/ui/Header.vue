6cf143fd51332ad6d7ad1e23d343fe08abfeb252
Fransuelton <elton6103@gmail.com>
2025-08-13 13:45:58 -0300

    ✨ feat: create Header component with logo and styling

---
+++ src/components/ui/Header.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <header class="header">
+    <img
+      src="../../assets/images/logo.svg"
+      alt="Base Apparel Logo"
+      title="Base Apparel Logo"
+      class="logo"
+    />
+  </header>
+</template>
+
+<style scoped>
+.header {
+  padding: 3.2rem;
+}
+.header .logo {
+  width: 10rem;
+  height: 2.1rem;
+}
+</style>
